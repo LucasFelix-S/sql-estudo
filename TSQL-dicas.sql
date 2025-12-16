20da9a069d4b4d1e67c4b6891bdf5290f842a4b2
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2025-12-15 21:38:29 -0300

    docs: adicionando dica para manipulação de CTE's em T-SQL

--- TSQL-dicas.sql
+++ TSQL-dicas.sql
@@ -2,7 +2,15 @@
 --[PLANO DE EXECUÇÃO ESTIMADO]--
 --------------------------------
 /*
-	O Plano de execução estimado serve para mostrar como o SQL Server pretende executar a consulta,
+O Plano de execução estimado serve para mostrar como o SQL Server pretende executar a consulta,
 ela nos ajuda a identificar gargalos, sugestões de melhorias, comparar estratégias, ou seja, é o raio-x pré-execução
-	Para executá-lo podemos usar o CTRL + L, ou clicar no ícone próximo ao botão de "executar" no SSMS
+Para executá-lo podemos usar o CTRL + L, ou clicar no ícone próximo ao botão de "executar" no SSMS.
+*/
+
+-------------------
+--[USO DAS CTE'S]--
+-------------------
+/*
+A CTE sempre precisa ser usada logo em seguida por um SELECT, INSERT, UPDATE ou DELETE. Se não você só cria a CTE e 
+o SGBD "não sabe" o que fazer com ela após a sua criação.
 */
