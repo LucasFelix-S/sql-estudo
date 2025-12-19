da38ee809a4f72a136013ab7e2aa8f81bcbea5b2
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2025-12-18 21:51:25 -0300

    docs: adicionando dicas para o aprendizado em Window Functions

--- TSQL-dicas.sql
+++ TSQL-dicas.sql
@@ -21,3 +21,19 @@ o SGBD "não sabe" o que fazer com ela após a sua criação.
 /*
 Sempre tomar cuidado com LOOPS infinitos, analisar anteriormente para ver se as variáveis estão sendo atualizadas corretamente.
 */
+
+------------------------
+-- [WINDOW FUNCTIONS] --
+------------------------
+/*
+Para que server?
+-> São utilizadas para cálculos mais avançados de Análise de Dados;
+-> Possuem uso semelhante ao GROUP BY, só que mais avançado e personalizável;
+-> A instrução OVER permite definir qual será a "janela" (conjunto de linhas) a ser considerado no cálculo;
+-> A instrução PARTITION BY divide o conjunto em partições nas quais as fun~ções de janela são aplicadas.
+
+As funções de janela possuem as seguintes finalidades:
+1. Cálculos de agragação: COUNT, SUM, AVG, MIN, MAX...
+2. Cálculos de deslocamento: FIRST_VALUE, LAST_VALUE, LEAD, LAG
+3. Cálculos estatísticos: RANK, DENSE_RANK, NTILE
+*/
